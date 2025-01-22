f383940b6d5e836c281b8010ab0ac82c629bffab
Siberian <vasileios.kritikos@aueb.gr>
2025-01-22 14:34:30 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Person } from './shared/interfaces/person';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,17 +19,17 @@ export class AppComponent {
     email:"example@example.gr"
   }
 
-  person0 = {
+  person0: Person = {
     givenName: "Bill00",
     surName: "Siberian00",
     age: 90,
     email:"example@example00.com"
   }
 
-  person1 = {
+  person1: Person = {
     givenName: "Bill11",
     surName: "Siberian11",
-    age: 9,
+    age: 11,
     email:"example@example11.com"
   }
 }
